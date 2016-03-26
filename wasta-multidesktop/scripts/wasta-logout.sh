d8f8dba4859d71d68464c4993b010fc2c1d674d3
Rik Shaw <rikshaw76@gmail.com>
2016-03-26 09:33:24 +0300

    sync backgrounds on logout

--- wasta-multidesktop/scripts/wasta-logout.sh
+++ wasta-multidesktop/scripts/wasta-logout.sh
@@ -5,32 +5,13 @@
 #   This script is intended to run by lightdm at logout.
 #
 #   2016-03-16 rik: initial script for 16.04
+#   2016-03-26 rik: syncing user's cinnamon / gnome backgrounds on logout
 #
 # ==============================================================================
 
-#NEMO_ACTIVE=$(pidof nemo)
-#NAUTILUS_ACTIVE=$(pidof nautilus)
-
-#if [ "$NEMO_ACTIVE" ];
-#then
-#    # sync Cinnamon background to GNOME background
-#    CINNAMON_BACKGROUND=$(su "$USER" -c gsettings get org.cinnamon.desktop.background picture-uri)
-#    su "$USER" -c "gsettings set org.gnome.desktop.background picture-uri $CINNAMON_BACKGROUND"
-#fi
-
-#if [ "$NAUTILUS_ACTIVE" ];
-#then
-#    # sync GNOME background to Cinnamon background
-#    GNOME_BACKGROUND=$(su "$USER" -c gsettings get org.gnome.desktop.background picture-uri)
-#    su "$USER" -c "gsettings set org.cinnamon.desktop.background picture-uri $GNOME_BACKGROUND"
-#fi
-
-# File manager defaults set to Nautilus, since Unity will not restart
-# correctly when attempting to change from nemo and nautilus at login.
-#
-# Effectively for Cinnamon/Nemo users each time they login the defaults
-# will be for Nautilus and then be re-toggled to Nemo after the
-# wasta-login.sh script completes.
+# ------------------------------------------------------------------------------
+# All Session Fixes
+# ------------------------------------------------------------------------------
 
 # Prevent Nemo from drawing the desktop
 su "$USER" -c 'gsettings set org.nemo.desktop show-desktop-icons false'
@@ -39,5 +20,27 @@ su "$USER" -c 'gsettings set org.nemo.desktop show-desktop-icons false'
 su "$USER" -c 'gsettings set org.gnome.settings-daemon.plugins.background active true'
 su "$USER" -c 'gsettings set org.gnome.desktop.background draw-background true'
 su "$USER" -c 'gsettings set org.gnome.desktop.background show-desktop-icons true'
+
+# ------------------------------------------------------------------------------
+# Processing based on active Window Manager
+# ------------------------------------------------------------------------------
+
+# WHEN UNITY ACTIVE, normally wmctrl -m returns "Compiz" but here it is not
+# showing, just empty wmctrl -- maybe it gets unloaded sooner or something?
+# Anyway, will have to adjust based only on Muffin found or not then
+
+MUFFIN_ACTIVE=$(wmctrl -m | grep Muffin)
+# UNITY_ACTIVE$(wmctrl -m | grep Compiz)
+
+if [ "$MUFFIN_ACTIVE" ];
+then
+    # sync Cinnamon background to GNOME background
+    CINNAMON_BACKGROUND=$(su "$USER" -c 'gsettings get org.cinnamon.desktop.background picture-uri')
+    su "$USER" -c 'gsettings set org.gnome.desktop.background picture-uri $CINNAMON_BACKGROUND'
+else
+    # sync GNOME background to Cinnamon background
+    GNOME_BACKGROUND=$(su "$USER" -c 'gsettings get org.gnome.desktop.background picture-uri')
+    su "$USER" -c 'gsettings set org.cinnamon.desktop.background picture-uri $GNOME_BACKGROUND'
+fi
 
 exit 0
