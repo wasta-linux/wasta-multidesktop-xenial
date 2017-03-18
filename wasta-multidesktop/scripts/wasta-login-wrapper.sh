a76a2cfaff39bed4773c623db56586e3c709831c
Rik Shaw <rikshaw76@gmail.com>
2017-03-18 10:46:08 +0300

    wasta-login: no longer triggering with 'at'

--- wasta-multidesktop/scripts/wasta-login-wrapper.sh
+++ wasta-multidesktop/scripts/wasta-login-wrapper.sh
@@ -11,13 +11,20 @@
 #   2013-12-21 rik: initial script
 #   2016-11-07 rik: updating to use "at" to call "real script", otherwise
 #       in newer releases the login process waits for completion.
+#   2017-03-18 riK: removing "at" trigger, so this script won't complete until
+#       after wasta-login completes, but this is good to not create confusion.
 #
 # ==============================================================================
 
-if ! [ -e /var/spool/cron/atjobs/.SEQ ]; then
-  /bin/bash -c "/usr/share/wasta-multidesktop/scripts/wasta-login.sh $*" &
-else
-  echo "/usr/share/wasta-multidesktop/scripts/wasta-login.sh $*" | at now
-fi
+#if ! [ -e /var/spool/cron/atjobs/.SEQ ]; then
+#  /bin/bash -c "/usr/share/wasta-multidesktop/scripts/wasta-login.sh $*" &
+#else
+#  echo "/usr/share/wasta-multidesktop/scripts/wasta-login.sh $*" | at now
+#fi
+
+# rik: removing 'at' triggering since we don't need wasta-login to wait
+#   anymore and when it is triggered asynchronously it causes some confusion
+#   So, this wrapper isn't really needed anymore but am keeping for now.
+/bin/bash -c "/usr/share/wasta-multidesktop/scripts/wasta-login.sh $*"
 
 exit 0
